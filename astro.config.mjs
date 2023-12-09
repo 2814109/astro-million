9edae3d40a4d9efee6746165895c2f5daa963b4f
2814109 <2814109k@gmail.com>
2023-12-09 19:44:03 +0900

    add millionJS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+import million from 'million/compiler';
+ 
+export default defineConfig({
+  vite: {
+    plugins: [million.vite({ mode: 'react', server: true, auto: true })]
+  }
+});
