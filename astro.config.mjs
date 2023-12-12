ccb3db1d9d5dbf4b105076e5a8b510ce85579248
2814109 <2814109k@gmail.com>
2023-12-12 22:58:33 +0900

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // import { defineConfig } from 'astro/config';
 
 // // https://astro.build/config
-// export default defineConfig({});
+// 
+// https://astro.build/config
+export default defineConfig({});
 
 import { defineConfig } from 'astro/config';
 import million from 'million/compiler';
- 
+import react from "@astrojs/react";
 export default defineConfig({
   vite: {
-    plugins: [million.vite({ mode: 'react', server: true, auto: true })]
-  }
+    plugins: [million.vite({
+      mode: 'react',
+      server: true,
+      auto: true
+    })]
+  },
+  integrations: [react()]
 });
